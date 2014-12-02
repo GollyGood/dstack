ff654fb52f6d98100e000808d1b5cf582b3b826a
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2014-12-02 08:40:12 -0500

    Changes made for quick demo

--- cookbooks/local-cookbooks/default-web/recipes/default.rb
+++ cookbooks/local-cookbooks/default-web/recipes/default.rb
@@ -29,6 +29,15 @@ vlamp['sites'].each_pair do |key, value|
     server_aliases aliases
     server_name server_name
   end
+
+  # Append configured domain to /etc/hosts file.
+  ruby_block "append_hosts" do
+    block do
+      file = Chef::Util::FileEdit.new("/etc/hosts")
+      file.insert_line_if_no_match("127.0.0.1\t#{server_name}", "127.0.0.1\t#{server_name}")
+      file.write_file
+    end
+  end
 end
 
 vlamp['databases'].each do |database_name|
@@ -41,3 +50,21 @@ vlamp['databases'].each do |database_name|
     action :create
   end
 end
+
+php_pear "Console_Table" do
+  action :install
+end
+
+# Initialize drush PEAR channel
+dc = php_pear_channel "pear.drush.org" do
+  action :discover
+end
+
+# Install drush
+php_pear "drush" do
+  version '6.2.0.0'
+  channel dc.channel_name
+  action :install
+end
+
+
