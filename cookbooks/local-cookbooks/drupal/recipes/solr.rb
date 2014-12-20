77d8e8c13c699fbf31f6c78f994cd0babffbc271
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2014-12-20 09:58:25 -0500

    Creating ApacheSolr specific utiliy recipe and Drupal ApacheSolr specific recipe

---
+++ cookbooks/local-cookbooks/drupal/recipes/solr.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: drupal
+# Recipe:: solr
+#
+# Copyright 2014 dStack Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'utils::solr'
+
+module_conf_dir = node['drupal']['solr']['module_conf_dir']
+apachesolr_conf_dir = node['drupal']['solr']['apachesolr_conf_dir']
+
+if node['drupal']['solr']['module_path'] == ''
+  raise 'Drupal ApacheSolr module directory has not be set.'
+elsif !File.directory? node['drupal']['solr']['module_path']
+  raise 'Unable to locate the Drupal ApacheSolr module directory. Please check the path.'
+end
+
+# Copy the Drupal ApacheSolr conf files to the ApacheSolr conf directory.
+node['drupal']['solr']['module_conf_files'].each do |config_file|
+  file "#{node['solr']['home']}/#{apachesolr_conf_dir}/#{config_file}" do
+    content IO.read("#{node['drupal']['solr']['module_path']}/#{module_conf_dir}/#{config_file}")
+    notifies :restart, 'service[jetty]'
+  end
+end
--- cookbooks/local-cookbooks/utils/metadata.rb
+++ cookbooks/local-cookbooks/utils/metadata.rb
@@ -7,4 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.1.0"
 recipe "utils::dev-tools", "Installs and configures development tools."
 recipe "utils::scripts", "Runs specified scripts at post-install and post-up."
+recipe "utils::solr", "Installs and configures ApacheSolr using Jetty."
 recipe "utils::xdebug", "Installs and configures xdebug."
+depends "hipsnip-solr"
